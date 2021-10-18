460326f8445dc0bdc93f0db884252e3ce800988e
Yejin Carol Chang <81130006+Yejin-Carol@users.noreply.github.com>
2021-10-18 23:58:37 +0900

    Create DaysTeller.java
    211018

---
+++ springch2/DaysTeller.java
@@ -0,0 +1,52 @@
+package com.springcastello.ch2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DaysTeller {
+
+//	public static void main(String[] args) {http://localhost:8080/ch2/getDays?year=2021&month=10&day=19
+		//1. insert
+		@RequestMapping("/getDays")
+		public void main(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		String year = request.getParameter("year");
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+		
+		int yyyy = Integer.parseInt(year);
+		int mm = Integer.parseInt(month);
+		int dd = Integer.parseInt(day);
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(yyyy, mm -1, dd);
+		
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);//1: Sunday, 2: Monday...
+		char days = " 일월화수목금토".charAt(dayOfWeek);
+		
+		 // 3. 출력
+//	      System.out.println(year + "년 " + month + "월 " + day + "일은 ");
+//	      System.out.println(days + "요일입니다.");
+	      response.setContentType("text/html");    // 응답의 형식을 html로 지정
+	      response.setCharacterEncoding("utf-8");  // 응답의 인코딩을 utf-8로 지정
+	      PrintWriter out = response.getWriter();  // 브라우저로의 출력 스트림(out)을 얻는다.
+	      out.println("<html>");
+	      out.println("<head>");
+	      out.println("</head>");
+	      out.println("<body>");
+	      out.println(year + "년 " + month + "월 " + day + "일은 ");
+	      out.println(days + "요일입니다.");
+	      out.println("</body>");
+	      out.println("</html>");
+	      out.close();
+			
+	}
+
+}
